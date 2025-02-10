6e9425e8177360b7544f69c9ad1570d7933e767c
Nouri-redouane <nour1r3d1@gmail.com>
2025-02-10 22:18:01 +0100

    feat: add loading effect to image

--- frontend/src/components/summary_card.js
+++ frontend/src/components/summary_card.js
@@ -1,30 +1,50 @@
 import { EyeOutlined, HeartOutlined } from "@ant-design/icons";
-import { Card, Tooltip } from "antd";
-import React from "react";
+import { Card, Skeleton, Tooltip } from "antd";
+import React, { useEffect, useState } from "react";
 import { TMDB_IMAGES_BASE_URL } from "../utils/constants";
 
 const { Meta } = Card;
 
-const SummaryCard = ({ title, description, image_endpoint, is_loading }) => (
-  <Card
-    loading={is_loading}
-    className="flex-1 min-w-[250px] max-w-[300px] flex flex-col"
-    cover={
-      image_endpoint ? (
-        <img alt={title} src={`${TMDB_IMAGES_BASE_URL}${image_endpoint}`} />
-      ) : undefined
-    }
-    actions={[
-      <Tooltip key="inspect" title="Inspect">
-        <EyeOutlined />
-      </Tooltip>,
-      <Tooltip key="fav" title="Add to favorites">
-        <HeartOutlined />
-      </Tooltip>,
-    ]}
-  >
-    <Meta title={title} description={description} className="flex-1" />
-  </Card>
-);
+const SummaryCard = ({ title, description, image_endpoint, is_loading }) => {
+  const [is_image_loading, set_is_image_loading] = useState(false);
+
+  useEffect(() => {
+    set_is_image_loading(true);
+  }, [image_endpoint]);
+
+  return (
+    <Card
+      loading={is_loading}
+      className="flex-1 min-w-[250px] max-w-[300px] flex flex-col"
+      cover={
+        image_endpoint ? (
+          <div>
+            {(is_image_loading || is_loading) && (
+              <Skeleton.Image className="!w-full" active={true} />
+            )}
+            <img
+              alt={title}
+              className={`${
+                is_image_loading || is_loading ? "hidden" : "block"
+              }`}
+              src={`${TMDB_IMAGES_BASE_URL}${image_endpoint}`}
+              onLoad={() => set_is_image_loading(false)}
+            />
+          </div>
+        ) : undefined
+      }
+      actions={[
+        <Tooltip key="inspect" title="Inspect">
+          <EyeOutlined />
+        </Tooltip>,
+        <Tooltip key="fav" title="Add to favorites">
+          <HeartOutlined />
+        </Tooltip>,
+      ]}
+    >
+      <Meta title={title} description={description} className="flex-1" />
+    </Card>
+  );
+};
 
 export default SummaryCard;
